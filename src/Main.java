b144fe4fbde230b5d0942e0592372f6b281c88a4
Maier Georgiana <149155244+giamaier03777@users.noreply.github.com>
2025-02-18 12:11:46 +0200

    finished

--- src/Main.java
+++ src/Main.java
@@ -16,17 +16,27 @@ public class Main {
         ProduktRepo produktRepo = new ProduktRepo();
         ProduktController produktController = new ProduktController(produktRepo);
 
-        produktRepo.addProdukt(new Produkt("Mjolnir", 500, "Asgard"));
-        produktRepo.addProdukt(new Produkt("Vibranium-Schild", 700, "Wakanda"));
-        produktRepo.addProdukt(new Produkt("Infinity Gauntlet", 10000, "Titan"));
-        produktRepo.addProdukt(new Produkt("Web-Shooter", 250, "Terra"));
-        produktRepo.addProdukt(new Produkt("Arc-Reaktor", 1500, "Terra"));
-        produktRepo.addProdukt(new Produkt("Norn Stones", 1200, "Asgard"));
-        produktRepo.addProdukt(new Produkt("Quantum Suit", 3000, "Terra"));
-        produktRepo.addProdukt(new Produkt("X-Gene Serum", 850, "X-Mansion"));
-        produktRepo.addProdukt(new Produkt("Cosmic Cube", 9000, "Multiverse"));
-        produktRepo.addProdukt(new Produkt("Darkhold", 2000, "Multiverse"));
+//        produktRepo.addProdukt(new Produkt("Mjolnir", 500, "Asgard"));
+//        produktRepo.addProdukt(new Produkt("Vibranium-Schild", 700, "Wakanda"));
+//        produktRepo.addProdukt(new Produkt("Infinity Gauntlet", 10000, "Titan"));
+//        produktRepo.addProdukt(new Produkt("Web-Shooter", 250, "Terra"));
+//        produktRepo.addProdukt(new Produkt("Arc-Reaktor", 1500, "Terra"));
+//        produktRepo.addProdukt(new Produkt("Norn Stones", 1200, "Asgard"));
+//        produktRepo.addProdukt(new Produkt("Quantum Suit", 3000, "Terra"));
+//        produktRepo.addProdukt(new Produkt("X-Gene Serum", 850, "X-Mansion"));
+//        produktRepo.addProdukt(new Produkt("Cosmic Cube", 9000, "Multiverse"));
+//        produktRepo.addProdukt(new Produkt("Darkhold", 2000, "Multiverse"));
 
+        Produkt prod1 = new Produkt("Mjolnir", 500, "Asgard");
+        Produkt prod2 = new Produkt("Vibranium-Schild", 700, "Wakanda");
+        Produkt prod3 = new Produkt("Infinity Gauntlet", 10000, "Titan");
+        Produkt prod4 = new Produkt("Web-Shooter", 250, "Terra");
+        Produkt prod5 = new Produkt("Arc-Reaktor", 1500, "Terra");
+        Produkt prod6 = new Produkt("Norn Stones", 1200, "Asgard");
+        Produkt prod7 = new Produkt("Quantum Suit", 3000, "Terra");
+        Produkt prod8 = new Produkt("X-Gene Serum", 850, "X-Mansion");
+        Produkt prod9 = new Produkt("Cosmic Cube", 9000, "Multiverse");
+        Produkt prod10 = new Produkt("Darkhold", 2000, "Multiverse");
 
 
         Charakter c1 = new Charakter(1, "Thor", "Asgard");
@@ -35,12 +45,26 @@ public class Main {
         Charakter c4 = new Charakter(4, "Spider-Man", "Terra");
         Charakter c5 = new Charakter(5, "Doctor Strange", "Multiverse");
 
+        c1.addProdukt(prod1);
+        c1.addProdukt(prod6);
+        c1.addProdukt(prod10);
 
-        charakterRepo.addCharakter(c1);
-        charakterRepo.addCharakter(c2);
-        charakterRepo.addCharakter(c3);
-        charakterRepo.addCharakter(c4);
-        charakterRepo.addCharakter(c5);
+        c2.addProdukt(prod2);
+        c2.addProdukt(prod7);
+
+
+        c3.addProdukt(prod5);
+        c3.addProdukt(prod7);
+        c3.addProdukt(prod4);
+
+
+        c4.addProdukt(prod4);
+        c4.addProdukt(prod9);
+
+        c5.addProdukt(prod10);
+        c5.addProdukt(prod9);
+        c5.addProdukt(prod3);
+
 
         UI ui = new UI(charakterController, produktController);
         ui.start();
