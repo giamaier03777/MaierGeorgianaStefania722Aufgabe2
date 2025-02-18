b821571d448a935eb5169e5cfe8471074be57bbb
Maier Georgiana <149155244+giamaier03777@users.noreply.github.com>
2025-02-18 10:49:07 +0200

    Created Domain

--- src/Domain/Charakter.java
+++ src/Domain/Charakter.java
@@ -1,4 +1,77 @@
 package src.Domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Charakter {
+    private int id;
+    private String name;
+    private String region;
+    private List<Produkt> produkteListe;
+
+    /**
+     * Constructs a new Charakter object with the specified parameters.
+     *
+     * @param id   the id of the character
+     * @param name the name of the character
+     * @param region  the region of the character
+     */
+    public Charakter(int id, String name, String region) {
+        this.id = id;
+        this.name = name;
+        this.region = region;
+        this.produkteListe= new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public List<Produkt> getProdukteListe() {
+        return produkteListe;
+    }
+
+    public void setProdukteListe(List<Produkt> produkteListe) {
+        this.produkteListe = produkteListe;
+    }
+
+    public void addProdukt(Produkt produkt) {
+        produkteListe.add(produkt);
+    }
+
+    public void removeProdukt(Produkt produkt) {
+        produkteListe.remove(produkt);
+    }
+
+    @Override
+    public String toString() {
+        return "Charakter{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", produkteListe=" + produkteListe +
+                '}';
+    }
 }
+
--- src/Domain/Produkt.java
+++ src/Domain/Produkt.java
@@ -1,4 +1,59 @@
 package src.Domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Produkt {
+
+    private String name;
+    private int preis;
+    private String universum;
+
+    /**
+     * Constructs a new Medikament object with the specified parameters.
+     *
+     * @param name   the name of the medicine
+     * @param preis the price of the medicine
+     * @param universum  the disese of the medicine
+     */
+    public Produkt(String name, int preis, String universum) {
+        this.name = name;
+        this.preis = preis;
+        this.universum = universum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPreis() {
+        return preis;
+    }
+
+    public void setPreis(int preis) {
+        this.preis = preis;
+    }
+
+    public String getUniversum() {
+        return universum;
+    }
+
+    public void setUniversum(String universum) {
+        this.universum = universum;
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt{" +
+                "name='" + name + '\'' +
+                ", preis=" + preis +
+                ", universum='" + universum + '\'' +
+                '}';
+    }
 }
+
+
