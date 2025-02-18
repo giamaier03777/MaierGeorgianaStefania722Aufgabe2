25a9276044c240975263e2c29a1548d89e72a46f
Maier Georgiana <149155244+giamaier03777@users.noreply.github.com>
2025-02-18 10:59:55 +0200

    Created Repository

--- src/Repository/CharakterRepo.java
+++ src/Repository/CharakterRepo.java
@@ -1,4 +1,51 @@
 package src.Repository;
+import java.util.*;
+
+import src.Domain.Produkt;
+import src.Domain.Charakter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CharakterRepo {
+    private List<Charakter> charakterList;
+
+
+    public CharakterRepo() {
+        charakterList = new ArrayList<>();
+    }
+
+
+    public List<Charakter> getCharakterList() {
+        return charakterList;
+    }
+
+    public void addCharakter(Charakter charakter) {
+        charakterList.add(charakter);
+    }
+
+    public void removeCharakter(Charakter charakter) {
+        charakterList.remove(charakter);
+    }
+
+    public void getAllCharakter() {
+        for (Charakter charakter : charakterList) {
+            System.out.println(charakter);
+        }
+    }
+
+    public void updateCharakter(Charakter charakter) {
+        charakterList.remove(charakter);
+        charakterList.add(charakter);
+    }
+
+    public Charakter findCharacterbyId(int id) {
+        for (Charakter charakter : charakterList) {
+            if (charakter.getId() == id) {
+                return charakter;
+            }
+        }
+        return null;
+    }
 }
--- src/Repository/ProduktRepo.java
+++ src/Repository/ProduktRepo.java
@@ -1,4 +1,43 @@
 package src.Repository;
 
+
+import src.Domain.Produkt;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProduktRepo {
+    private List<Produkt> produktList;
+
+    public ProduktRepo() {
+        produktList = new ArrayList<>();
+    }
+
+    public void addProdukt(Produkt produkt) {
+        produktList.add(produkt);
+    }
+
+    public void removeProdukt(Produkt produkt) {
+        produktList.remove(produkt);
+    }
+
+    public List<Produkt> getAllProdukt() {
+        return produktList;
+    }
+
+    public void updateProdukt(Produkt produkt) {
+        produktList.remove(produkt);
+        produktList.add(produkt);
+    }
+
+    public Produkt findProdukttByName(String name) {
+        for (Produkt produkt : produktList) {
+            if (produkt.getName() == name) {
+                return produkt;
+            }
+        }
+        return null;
+    }
 }
+
+
